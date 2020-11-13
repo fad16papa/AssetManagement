bea28327d629f3a47195a276e28fb3c9928812a4
fad16papa <fad16papa@gmail.com>
2020-11-13 17:17:25 +0800

    Add Register in Application.User | Add ValidatonExtension in Application.Validators

---
+++ Application/User/Register.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Errors;
+using Application.Interfaces;
+using Application.Validators;
+using Domain;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.User
+{
+ public class Register
+    {
+        public class Command : IRequest<User>
+        {
+            public string UserName { get; set; }
+            public string Email { get; set; }
+            public string Password { get; set; }
+            public string DisplayName { get; set; }
+        }
+
+        public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+                RuleFor(x => x.UserName).NotEmpty();
+                RuleFor(x => x.Email).NotEmpty().EmailAddress();
+                RuleFor(x => x.Password).Password();
+                RuleFor(x => x.DisplayName).Password();
+            }
+        }
+
+        public class Handler : IRequestHandler<Command, User>
+        {
+            private readonly DataContext _context;
+            private readonly UserManager<AppUser> _userManager;
+            private readonly IJwtGenerator _jwtGenerator;
+
+            public Handler(DataContext context, UserManager<AppUser> userManager, IJwtGenerator jwtGenerator)
+            {
+                _context = context;
+                _userManager = userManager;
+                _jwtGenerator = jwtGenerator;
+            }
+
+            public async Task<User> Handle(Command request, CancellationToken cancellationToken)
+            {
+                //logic goes here
+                if (await _context.Users.AnyAsync(x => x.Email == request.Email))
+                {
+                    throw new RestException(HttpStatusCode.Conflict, string.Format($"Email {request.Email} already exist."));
+                }
+
+                if (await _context.Users.AnyAsync(x => x.UserName == request.UserName))
+                {
+                    throw new RestException(HttpStatusCode.Conflict, string.Format($"UserName {request.UserName} already exist."));
+                }
+
+                var user = new AppUser
+                {
+                    DisplayName = request.DisplayName,
+                    Email = request.Email,
+                    UserName = request.UserName,
+                };
+
+                var result = await _userManager.CreateAsync(user, request.Password);
+
+                if (result.Succeeded)
+                {
+                    return new User
+                    {
+                        DisplayName = user.DisplayName,
+                        Token = _jwtGenerator.CreateToken(user),
+                        UserName = user.UserName
+                    };
+                }
+                else
+                {
+                    throw new Exception("Problem creating user");
+                }
+            }
+        }
+    }
+}
