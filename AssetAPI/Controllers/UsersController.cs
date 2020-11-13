440cae666d0564c0f98ff7ee7984c153efe6858b
fad16papa <fad16papa@gmail.com>
2020-11-13 17:27:55 +0800

    Add UserController to AssetAPI

---
+++ AssetAPI/Controllers/UsersController.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Application.User;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AssetAPI.Controllers
+{
+    public class UsersController : BaseController
+    {
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(Register.Command command)
+        {
+            return await Mediator.Send(command);
+        }
+    }
+}
