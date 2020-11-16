313619d2594084353dc8df38406eda643cf1f62b
fad16papa <fad16papa@gmail.com>
2020-11-16 17:18:56 +0800

    Updating Migrations

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -12,32 +12,11 @@ namespace Persistence
 
         public DbSet<Asset> Assets { get; set; }
         public DbSet<License> Licenses { get; set; }
-        public DbSet<AssetHistory> AssetHistories { get; set; }
-        public DbSet<LicenseHistory> LicenseHistories { get; set; }
+        public DbSet<UserAssets> UserAssets { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
-
-            builder.Entity<AssetHistory>(ah =>
-            {
-                ah.HasKey(k => new { k.AppUserId, k.AssetId });
-
-                ah.HasOne(u => u.AppUser)
-                .WithMany(a => a.AssetHistories)
-                .HasForeignKey(u => u.AppUserId)
-                .OnDelete(DeleteBehavior.Restrict);
-            });
-
-            builder.Entity<LicenseHistory>(lh =>
-            {
-                lh.HasKey(k => new { k.AppUserId, k.LicenseId });
-
-                lh.HasOne(u => u.AppUser)
-                .WithMany(a => a.LicenseHistories)
-                .HasForeignKey(u => u.AppUserId)
-                .OnDelete(DeleteBehavior.Restrict);
-            });
         }
     }
 }
