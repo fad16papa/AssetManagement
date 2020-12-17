69fe3570c48ec76c7548f61bdfe384eb5f52a03c
fad16papa <fad16papa@gmail.com>
2020-12-17 15:37:14 +0800

    Commit changes

---
+++ AssetManagementWeb/Models/DTO/UserAssetsDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssetManagementWeb.Models.DTO
+{
+    public class UserAssetsDTO
+    {
+        public Guid AssetsId { get; set; }
+        public virtual AssetsDTO AssetsDTO { get; set; }
+        public Guid UserStaffId { get; set; }
+        public virtual UserStaffDTO UserStaffDTO { get; set; }
+        public DateTime IssuedOn { get; set; }
+        public DateTime ReturnedOn { get; set; }
+    }
+}
