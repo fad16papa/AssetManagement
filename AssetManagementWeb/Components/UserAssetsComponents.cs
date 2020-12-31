91ac92e36aeb578b44f51e668f0864ed6f1ffcee
fad16papa <fad16papa@gmail.com>
2020-12-31 14:49:47 +0800

    Commit changes

--- AssetManagementWeb/Components/UserAssetsComponents.cs
+++ AssetManagementWeb/Components/UserAssetsComponents.cs
@@ -26,7 +26,7 @@ namespace AssetManagementWeb.Components
             //Map the objects results to corresponding DTO's
             IEnumerable<UserAssets> userAssets = _mapper.Map<IEnumerable<UserAssets>>(result);
 
-            var resultUserAssets = userAssets.OrderByDescending(x => x.IssuedOn).Where(x => x.IsActive == isActive && x.Asset.IsAssinged == isActive).ToList();
+            var resultUserAssets = userAssets.OrderByDescending(x => x.IssuedOn).Where(x => x.IsActive == isActive && x.Asset.IsAssinged == isActive).ToList().Take(5);
 
             ViewBag.TotalUserAssetsCount = resultUserAssets.Count();
             ViewBag.isActive = isActive;
