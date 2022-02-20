11ed07ef7e223a5fef7ed21889189a20ec945b30
fad16papa <fad16papa@gmail.com>
2022-02-21 00:44:07 +0800

    Commit changes

--- Infrastructure/Security/JwtGenerator.cs
+++ Infrastructure/Security/JwtGenerator.cs
@@ -30,8 +30,8 @@ namespace Infrastructure.Security
             var tokenDescriptor = new SecurityTokenDescriptor
             {
                 Subject = new ClaimsIdentity(claims),
-                Expires = DateTime.Now.AddDays(7),
-                SigningCredentials = creds
+                Expires = DateTime.Now.AddHours(7),
+                SigningCredentials = creds,
             };
 
             var tokenHandler = new JwtSecurityTokenHandler();
